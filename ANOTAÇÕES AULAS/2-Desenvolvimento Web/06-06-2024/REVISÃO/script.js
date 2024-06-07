5c1502ef4e570a8d744d30411d03b34f4db81fd1
Felipe Spider <feliperobinsonprofissional@gmail.com>
2024-06-06 21:15:54 -0300

    Update

---
+++ ANOTAÇÕES AULAS/2-Desenvolvimento Web/06-06-2024/REVISÃO/script.js
@@ -0,0 +1,14 @@
+function formatacaoElementos(frmExemplo) {
+    var titulo = document.getElementById("titulo");
+    titulo.innerHTML = "REVISÃO PORRA";
+    titulo.style.background = "red";
+    titulo.style.fontSize = "30px";
+    titulo.style.color = "black";
+    var nome = document.getElementById("txt_Nome");
+    nome.style.color = "black";
+    nome.style.fontSize = "10px";
+    nome.style.borderColor = "brown";
+    nome.style.borderRadius = "1px";
+    var conteudo = document.frmExemplo.txt_Nome.value;
+    alert(conteudo);
+}
