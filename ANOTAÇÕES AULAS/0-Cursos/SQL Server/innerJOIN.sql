dafbc5f320223edf3106603047483262d5dc22c6
Felipe Spider <feliperobinsonprofissional@gmail.com>
2024-06-09 13:25:19 -0300

    Update innerJOIN.sql

--- ANOTAÇÕES AULAS/0-Cursos/SQL Server/innerJOIN.sql
+++ ANOTAÇÕES AULAS/0-Cursos/SQL Server/innerJOIN.sql
@@ -1,15 +1,42 @@
 create table FAMILIA (
     Id int identity,
-    Nome int not null,
+    Nome Varchar(10) not null,
     Idade int
 
     CONSTRAINT pk_FAMILIA_Id PRIMARY KEY (Id)
 )
 
+INSERT FAMILIA (Nome, Idade)
+VALUES ('Felipe','15'),
+       ('Manoela','14'),
+       ('Ellen', '43'),
+       ('Tomaz', '80'),
+       ('Silveira','16')
+
+
 create table AMIGOS (
     Id int identity,
-    Nome int not null,
-    Idade int not null
+    Nome Varchar(10) not null,
+    Idade int
 
     CONSTRAINT pk_AMIGOS_Id PRIMARY KEY (Id)
 )
+
+INSERT AMIGOS (Nome, Idade)
+VALUES ('Diogo','16'),
+       ('Joãozin','15'),
+       ('João Pedro', '16'),
+       ('João Vini', '16')
+
+
+SELECT * FROM FAMILIA
+SELECT * FROM AMIGOS
+
+
+SELECT FAMILIA.Idade = a, AMIGOS.Idade = b 
+FROM 
+WHERE Idade > 16
+
+
+drop table FAMILIA
+drop table AMIGOS
