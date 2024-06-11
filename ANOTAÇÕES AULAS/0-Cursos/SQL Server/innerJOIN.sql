45b4ebc8edfc6792ec2a902f48dc2ebf30c8f0c0
Felipe Spider <feliperobinsonprofissional@gmail.com>
2024-06-11 19:37:35 -0300

    Update innerJOIN.sql

--- ANOTAÇÕES AULAS/0-Cursos/SQL Server/innerJOIN.sql
+++ ANOTAÇÕES AULAS/0-Cursos/SQL Server/innerJOIN.sql
@@ -6,7 +6,7 @@ create table FAMILIA (
     CONSTRAINT pk_FAMILIA_Id PRIMARY KEY (Id)
 )
 
-INSERT FAMILIA (Nome, Idade)
+INSERT FAMILIA (Nome, Idade, )
 VALUES ('Felipe','15'),
        ('Manoela','14'),
        ('Ellen', '43'),
@@ -16,10 +16,23 @@ VALUES ('Felipe','15'),
 
 create table AMIGOS (
     Id int identity,
+    id_AMIGOS FOREIGN KEY
     Nome Varchar(10) not null,
     Idade int
 
     CONSTRAINT pk_AMIGOS_Id PRIMARY KEY (Id)
+)
+
+create table ADA (
+    Id int identity,
+    MeuAmigo varchar(500) not null,
+    id_AMIGOS VarChar(100) not null,
+    Nome Varchar(10) not null,
+    Idade int
+
+    CONSTRAINT pk_AMIGOS_Id PRIMARY KEY (Id)
+    CONSTRAINT fk_ADA_AMIGOS FOREIGN KEY AMIGOS REFERENCES (Id)
+    CONSTRAINT fk
 )
 
 INSERT AMIGOS (Nome, Idade)
@@ -27,6 +40,7 @@ VALUES ('Diogo','16'),
        ('Joãozin','15'),
        ('João Pedro', '16'),
        ('João Vini', '16'),
+       ('João Vini', '20'),
        ('Thiaguinho','19')
 
 
@@ -34,10 +48,26 @@ SELECT * FROM FAMILIA
 SELECT * FROM AMIGOS
 
 
-SELECT Idade, Idade 
-FROM FAMILIA , AmIGOS
-WHERE Idade > 16
+SELECT A.id_AMIGOS, 
+       Count(F.Id)
+  FROM FAMILIA F
+    INNER JOIN AMIGOS A 
+            ON A.id_AMIGOS = F.id
+GROUP BY F.Nome
+ORDER BY A.Id 
 
 
-drop table FAMILIA
-drop table AMIGOS
+SELECT A.id_AMIGOS as "", 
+       Count(F.Id)
+  FROM FAMILIA F
+    INNER JOIN AMIGOS A 
+            ON A.id_AMIGOS = F.id
+    INNER JOIN amigos_de_amigos ADA
+            ON ADA.Meuamigo = A.Id
+
+
+
+/* 
+COUNT() serve para contar
+GROUP BY serve como se fosse um WHERE, ou armazenar um elemento repetido
+*/
