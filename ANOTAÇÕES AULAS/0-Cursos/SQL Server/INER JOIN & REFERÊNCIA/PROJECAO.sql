3eb699f3ede534323d777e46f73138944b1c701c
Felipe Spider <feliprerobinsonprofissional@gmail.com>
2024-06-12 21:50:54 -0300

    Update

---
+++ ANOTAÇÕES AULAS/0-Cursos/SQL Server/INER JOIN & REFERÊNCIA/PROJECAO.sql
@@ -0,0 +1,8 @@
+/* Alias de coluna = Nome hiportético a seleções (Criado dentro do select (função + as + NOME)) */
+
+
+/*
+SELECT = As colunas a serem selecionadas
+  FROM = De que tabela são essas colunas
+ WHERE = Condição para a seleção
+*/
