5284e4b9abf3c3da80bb4c41c2111d51c821f3a0
Felipe Spider <feliperobinsonprofissional@gmail.com>
2024-06-17 22:35:34 -0300

    Update

---
+++ ANOTAÇÕES AULAS/0-Cursos/Python do Zero/STRING/string.py
@@ -0,0 +1,11 @@
+nome = 'Felipe'
+idade = '15'
+profiss = 'Desempregado'
+
+texto = 'Oi, eu sou o ' + nome + ' tenho ' + idade + ' anos de idade e eu atualmente estou ' + profiss
+print(texto)
+
+# ou
+
+texto2 = f'Oi, eu sou o {nome} tenho {idade} anos de idade e eu estou atualmente {profiss}'
+print(texto2)
---
+++ ANOTAÇÕES AULAS/0-Cursos/Python do Zero/TUPLA/tupla.py
@@ -0,0 +1,5 @@
+thistuple = tuple(("apple", "banana", "cherry"))
+# print(len(thistuple))
+
+for a in range(0, len(thistuple)):
+	print(a)
