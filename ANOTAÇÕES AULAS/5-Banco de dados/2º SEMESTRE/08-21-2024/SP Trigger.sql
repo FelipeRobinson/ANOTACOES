08dc32f8b05e918040ba116bb0f96d85d2092195
Felipe Spider <feliperobinsonprofissional@gmail.com>
2024-08-21 20:52:53 -0300

    Update
    undefined

---
+++ ANOTAÇÕES AULAS/5-Banco de dados/2º SEMESTRE/08-21-2024/SP Trigger.sql
@@ -0,0 +1,73 @@
+CREATE TABLE trCLIENTES(
+   RG char(15) not null,
+   Nome varchar(40) not null,
+   EMail varchar(40) not null,
+   Telefone varchar(40) not null,
+
+   constraint pk_clientes primary key  (rg)
+) 
+
+CREATE TABLE trAGENCIA(
+   id Int Identity not null,
+   Nome varchar(40) not null,
+
+   constraint pk_agencia primary key  (id)
+) 
+
+CREATE TABLE trCONTA(
+   idAgencia int not null,
+   nrConta int not null,
+   DV int not null,
+   Tipo int not null,
+   idCliente char(15) not null,
+   saldo money not null,
+
+   constraint pk_conta primary key  (idAgencia,nrConta),
+   constraint fk_conta_agencia foreign key (idAgencia) references trAgencia(id),
+   constraint fk_conta_cliente foreign key (idCliente) references trClientes(rg)
+) 
+
+CREATE TABLE trMOVIMENTACAO(
+   id Int Identity not null,
+   idAgencia int not null,
+   nrConta int not null,
+   data smalldatetime not null,
+   descricao varchar(50) not null,
+   valor  money not null,
+   credito bit not null,
+   
+   constraint pk_movimentacao primary key (id),
+	CONSTRAINT fk_mov_ag_conta FOREIGN KEY (idAgencia,nrCOnta) REFERENCES trCONTA (idAgencia,nrCOnta)
+)
+
+CREATE TABLE trAGENDAMENTO(
+   id Int Identity not null,
+   idAgencia int not null,
+   nrConta int not null,
+   data smalldatetime not null,
+   descricao varchar(50) not null,
+   valor  money not null,
+   credito bit not null,
+   
+   constraint pk_agendamento primary key (id),
+	CONSTRAINT fk_agendamento_ag_conta FOREIGN KEY (idAgencia,nrCOnta) REFERENCES trCONTA (idAgencia,nrCOnta)
+)
+
+INSERT INTO trCLIENTES 
+values ('52154','Marcelo Antonio Santos','mar@uol.com','36254587'),
+       ('23423','Gil Faber','gf211@uol.com','2345476'),
+       ('68767','Beth Brunella','bb@uol.com','32342347'),
+       ('12333','Cris Antunes','ca@uol.com','39789587')
+
+INSERT INTO trAGENCIA(nome) 
+values ('Barao de Itapura'),
+       ('Culto A Ciencia'),
+       ('Centro')
+
+INSERT INTO trCONTA 
+values (1,10,5,0,'52154',100),
+       (2,100,0,0,'23423',520),
+       (2,110,5,1,'68767',210),
+       (3,120,0,0,'12333',325)
+
+SELECT * FROM trCONTA 
---
+++ ANOTAÇÕES AULAS/5-Banco de dados/2º SEMESTRE/08-21-2024/excercise.sql
@@ -0,0 +1,104 @@
+-- 1)
+CREATE FUNCTION CalcularDigitoVerificador (@nrConta INT)
+RETURNS INT
+AS
+BEGIN
+    -- o DECLARE pode ser um valor (seja INT, CHAR, VARCHAR) e adicionamos um nome para poder 'seta-lo'
+    DECLARE @metadeDaConta INT;
+    DECLARE @digitoVerificado INT;
+
+    -- Aqui pegamos a declaração da metade do número da CONTA
+    SET @metadeDaConta = @nrConta / 2;
+
+    -- Aqui verificamos qual é o último número apos a metade ter sido definido
+    SET @digitoVerificado = @metadeDaConta % 10;
+
+    RETURN @digitoVerificado;
+END;
+
+
+-- 2)
+CREATE FUNCTION ObterSaldoNaData (
+    -- Obtendo os parâmetros --
+    @nrConta INT,
+    @idAgencia INT,
+    @data SMALLDATETIME
+)
+RETURNS MONEY
+AS
+BEGIN
+    -- DECLARANDO as VARIÁVEIS --
+    DECLARE @saldoInicial MONEY;
+    DECLARE @saldoFinal MONEY;
+    DECLARE @creditos MONEY;
+    DECLARE @debitos MONEY;
+
+    -- Pegando o saldo inicial da CONTA --
+    SELECT @saldoInicial = saldo
+      FROM trCONTA
+     WHERE nrConta = @nrConta AND idAgencia = @idAgencia;
+
+    -- Calculando o total de crédito até a data de referencia --
+    SELECT @creditos = ISNULL(SUM(valor), 0)
+      FROM trMOVIMENTACAO
+     WHERE nrConta = @nrConta AND idAgencia = @idAgencia
+           AND data <= @data
+           AND credito = 1;
+
+    -- Calculando o total de débito até a data de referência --
+    SELECT @debitos = ISNULL(SUM(valor), 0)
+      FROM trMOVIMENTACAO
+     WHERE nrConta = @nrConta AND idAgencia = @idAgencia
+           AND data <= @data
+           AND credito = 0;
+
+    -- Calculando o valor final, sendo ele a soma do saldo inicial e do crédito, e a subtração do débito pelo crédito --
+    SET @saldoFinal = @saldoInicial + @creditos - @debitos;
+
+    -- RETORNANDO O SALDO FINAL --
+    RETURN @saldoFinal;
+END;
+
+
+-- 3-)
+CREATE FUNCTION ObterValorCreditádoNaData (
+    -- Obtendo os parâmetros --
+    @nrConta INT,
+    @idAgencia INT,
+    @dataFutura SMALLDATETIME
+)
+RETURNS MONEY
+AS
+BEGIN
+    -- DECLARANDO as VARIÁVEIS --
+    DECLARE @saldoAtual MONEY;
+    DECLARE @saldoPrevisto MONEY;
+    DECLARE @creditosFuturos MONEY;
+    DECLARE @debitosFuturos MONEY;
+
+    -- Pegando o saldo atual da CONTA --
+    SELECT @saldoAtual = saldo
+      FROM trCONTA
+     WHERE nrConta = @nrConta AND idAgencia = @idAgencia;
+
+    -- Calculando o total de crédito até a data de referencia --
+    SELECT @creditosFuturos = ISNULL(SUM(valor), 0)
+      FROM trMOVIMENTACAO
+     WHERE nrConta = @nrConta AND idAgencia = @idAgencia
+           AND data > GETDATE()
+           AND data > @dataFutura
+           AND credito = 1;
+
+    -- Calculando o total de débito até a data de referência --
+    SELECT @debitosFuturos = ISNULL(SUM(valor), 0)
+      FROM trMOVIMENTACAO
+     WHERE nrConta = @nrConta AND idAgencia = @idAgencia
+           AND data > GETDATE() <= @dataFutura
+           AND credito = 0;
+
+    -- Calculando o valor final, sendo ele a soma do saldo inicial e do crédito, e a subtração do débito pelo crédito --
+    SET @saldoPrevisto = @saldoAtual + @creditosFuturos - @debitosFuturos;
+
+    -- RETORNANDO O SALDO FINAL --
+    RETURN @saldoPrevisto;
+END;
