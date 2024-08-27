f6a2d8cb4cb379ff4f4f893c5bbd1b30ce480b8d
Felipe Spider <feliperobinsonprofissional@gmail.com>
2024-08-26 22:10:43 -0300

    Update

--- ANOTAÇÕES AULAS/3-Técnicas de Programação/JAVA/08-24-2024/Data.java
+++ ANOTAÇÕES AULAS/3-Técnicas de Programação/JAVA/08-24-2024/Data.java
@@ -1,5 +1,4 @@
-public class Data
-{
+public class Data {
     private byte  dia, mes;
     private short ano;
 
@@ -13,28 +12,35 @@ public class Data
                 return false;
 
         // Calendario Gregoriano
-        if (ano%400==0) return true;
-        if (ano%  4==0 && ano%100!=0) return true;
+        if (ano%400==0) 
+            return true;
+        if (ano%  4==0 && ano%100!=0) 
+            return true;
         return false;
     }
 
-    public static boolean isValida (byte dia, byte mes, short ano)
-    {
-        if (ano<-45) return false; // antes do Calendario Juliano
-        if (ano== 0) return false; // nao existiu ano 0; do ano 1ac foi direto para o ano 1dc
-        if (ano==1582 && mes==10 && dia>=5 && dia<=14) return false; // dias cortados dos calendario pelo Papa Gregorio
+    public static boolean isValida (byte dia, byte mes, short ano) {
+        if (ano<-45) 
+            return false; 
+        if (ano== 0) 
+            return false;
+        if (ano==1582 && mes==10 && dia>=5 && dia<=14) 
+            return false;
 
-        if (dia<1 || dia>31 || mes<1 || mes>12) return false;
+        if (dia<1 || dia>31 || mes<1 || mes>12) 
+            return false;
 
-        if (dia>30 && (mes==4 || mes==6 || mes==9 || mes==11)) return false;
-        if (dia>29 && mes==2) return false;
-        if (dia>28 && mes==2 && !Data.isBissexto(ano)) return false;
+        if (dia>30 && (mes==4 || mes==6 || mes==9 || mes==11)) 
+            return false;
+        if (dia>29 && mes==2) 
+            return false;
+        if (dia>28 && mes==2 && !Data.isBissexto(ano)) 
+            return false;
 
         return true;
     }
 
-    public /*void*/ Data (byte dia, byte mes, short ano) throws Exception
-    {
+    public Data (byte dia, byte mes, short ano) throws Exception {
         if (!Data.isValida(dia,mes,ano))
             throw new Exception ("Data invalida");
 
@@ -43,79 +49,78 @@ public class Data
         this.ano=ano;
     }
 
-    public void setDia (byte dia) throws Exception
-    {
+    public void setDia (byte dia) throws Exception {
         if (!Data.isValida(dia,this.mes,this.ano))
             throw new Exception ("Dia invalido");
 
         this.dia=dia;
     }
 
-    public byte getDia ()
-    {
+    public byte getDia () {
         return this.dia;
     }
     
-    public void setMes (byte mes) throws Exception
-    {
+    public void setMes (byte mes) throws Exception {
         if (!Data.isValida(this.dia,mes,this.ano))
             throw new Exception ("Mes invalido");
 
         this.mes=mes;
     }
 
-    public byte getMes ()
-    {
+    public byte getMes () {
         return this.mes;
     }
     
-    public void setAno (short ano) throws Exception
-    {
+    public void setAno (short ano) throws Exception {
         if (!Data.isValida(this.dia,this.mes,ano))
             throw new Exception ("Ano invalido");
 
         this.ano=ano;
     }
 
-    public short getAno ()
-    {
+    public short getAno () {
         return this.ano;
     }
 
-    public void proximoDia() {
-        if (this.ano)
-        if (Data.isValida((byte) (this.dia + 1), this.mes, this.ano))
-            this.dia += 1;
-            // this.dia ++;
-        else if (Data.isValida((byte)1, (byte) (this.mes ++), ano))
+    public Data getDiaSeguinte () {
+        if (this.ano==1582 && this.mes==10 && this.dia==4) { 
+            try {
+            return new Data ((byte)15,(byte)10,(short)1582);
+            } 
+            catch (Exception erro1) {}
+        } 
+        else if (this.ano==-1 && this.mes==12 && this.dia==31) { 
+            try {
+                return new Data ((byte)1,(byte)1,(short)1);
+            } 
+            catch (Exception erro2){}} 
+        else { 
+            try {
+                return new Data (this.dia+1,this.mes,this.ano);
+            } 
+            catch (Exception erro3) { 
+                try {
+                    return new Data ((byte)1,this.mes+1,this.ano);
+                } 
+                catch (Exception erro4) { 
+                    try {
+                        return new Data ((byte)1,(byte)1,this.ano+1);
+                    } 
+                    catch (Exception erro4) {}
+                }
+            }
+        }
     }
 
-    public Data getDiaSeguinte ((byte)dia, (byte)mes, (short)ano) {
-/*      if (dia == 31)
-            return dia == '1';
-        if (dia == 31 && mes == 12)
-            return dia + '1'; 
-            return mes = '1';
-        if (mes == 2 && dia == 28)
-            return mes + 1;
-            return dia = '1';
-        if (mes == 4 || mes == 6 || mes == 9 || mes == 11 && dia == 30)
-            return mes + 1;
-            return dia = '1';
-        if (ano % 4 == 0)
-            if (dia == 31)
-                return dia + '1';
-            if (dia == 31 && mes == 12)
-                return dia + '1';
-                return mes = '1';
-            if (mes == 2 && dia == 28)
-                return mes + '1';
-                return dia = '1';
-            if (mes == '4' || mes == '6' || mes == '9' || mes == 11 && dia == 30)
-                return mes + '1';
-                return dia = '1';
-*/
-        if (proximoDia() = true)
+    public Data getVariosDiasAdiante (int qtd) throws Exception {
+        // faça
+    }
 
+    public Data getDiaAnterior () {
+        // faça
+    }
+
+    public Data getVariosDiasAtras (int qtd) throws Exception {
+        // faça
     }
 }
